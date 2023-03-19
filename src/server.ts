050ea657e1214b6f8fdf2cbe0c9ae3c5707da869
Pabloricheli <Pabloricheli@live.com>
2023-03-19 20:59:13 -0300

    new test

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,28 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
 
+const token = process.env.TOKEN || 'token'
+
 const app = express()
 app.use(express.json())
 
 // const prisma = new PrismaClient()
 
-app.get('/users', async (req, res) => {
-  // const user  = await prisma.user.findmany()
-
-  res.send('Hello world!')
+app.get('/', function (req, res) {
+  console.log(req)
 })
 
-app.post('/user', async (req, res) => {
-  const { name, phone } = req.body
+app.post('/', function (req, res) {
+  console.log(req.body)
+})
+
+app.get('/webhook', async (req, res) => {
+  console.log(req.body)
+})
+
+app.post('/webhook', async (req, res) => {
+  // const { name, phone } = req.body
+  console.log(req.body)
 })
 
 app.listen(process.env.PORT || 3333, () => {
