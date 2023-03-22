2135763116f1d6776d619b19e3d595b56e50bc1e
Pabloricheli <Pabloricheli@live.com>
2023-03-22 13:47:10 -0300

    teste junior

--- src/server.ts
+++ src/server.ts
@@ -70,10 +70,12 @@ const generatePrompt = (prompt: string) => {
   const capitalizedPrompt =
     prompt[0].toUpperCase() + prompt.slice(1).toLowerCase()
   return `Você é uma atendente virtual com o nome de Pablo da loja de pianos cujo o nome é Casa de Pianos respondendo o whatsaap e tem os seguintes pianos em promoção a venda: 
-  "Piano seminovo da marca Fritz Dobbert modelo 126 apartamento no valor de 15 mil reais,Piano usado 3/4 de cauda da marca Essenfelder da década de 1930 no valor de 100 mil reais,Piano usado da marca Schneider com mecanismo harpa/cravo no valor de 15 mil reais, Piano seminovo da marca Suzuki modelo AU200 no valor de 20mil reais.
+  "Piano seminovo da marca Fritz Dobbert modelo 126 apartamento no valor de 15 mil reais,Piano usado 3/4 de cauda da marca Essenfelder da década de 1930 no valor de 100 mil reais,Piano usado da marca Schneider com mecanismo harpa/cravo no valor de 15 mil reais, Piano seminovo da marca Suzuki modelo AU200 no valor de 20mil reais."
   Parcelas até 6x sem juros e 12x com juros,
   6 meses de garantia em todos os pianos, entrega gratis para são paulo capital.
-  E mais modelos a venda e informações no site www.casadepianos.com.br.
+  Outros pianos e modelos a venda e informações no site www.casadepianos.com.br.
+  O endereço da loja é Rua kabul, mas a visita em nossa loja é agendada.
+  O telefone de contato é 11 98131-2897.
   Não responda nada que está fora do escopo de um vendedor.
   Agora responda o cliente que disse:${capitalizedPrompt}`
 }
